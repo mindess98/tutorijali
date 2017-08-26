f4bac5ec65fd4a5a8f8525b5d9364f414f587eea
mindess98 <mindess98@gmail.com>
2017-08-26 16:07:53 +0200

    zadatak 4 sans input verification

---
+++ TP_1/zadatak_4/main.cpp
@@ -0,0 +1,50 @@
+/*
+U numerologiji starih Grka, svi prirodni brojevi su se dijelili u tri kategorije, prema tome kakva im je suma svih njihovih djelilaca (ne računajući njega samog). Oni brojevi kod kojih je ta suma
+manja od samog broja nazivali su se manjkavi (engl. deficient), oni kod kojih je ta suma veća od samog broja nazivali su se obilni (engl. abudant), dok su oni brojevi koji su jednaki sumi svih svojih
+djelilaca (bez njega samog) nazivali savršeni (engl. perfect). Recimo, broj 49 je manjkav: njegovi djelioci su 1 i 7, pri čemu je 1 + 7 = 8 < 49. Isto tako, broj 42 je obilan: njegovi djelioci su
+1,2, 3, 6, 7, 14 i 21, pri čemu je 1 + 2 + 3 + 6 + 7 + 14 + 21 = 54 > 42. Konačno, 28 je primjer savršenog broja: njegovi djelioci su 1, 2, 4, 7 i 14, a vrijedi 1 + 2 + 4 + 7 + 14 = 28.
+
+Napišite program koji traži da se sa tastature unese prirodan broj . U slučaju da korisnik unese nešto što nije prirodan broj (što uključuje i situaciju kada uneseni podatak uopće nije broj), treba
+ispisati poruku upozorenja, i ponoviti unos. Ukoliko je unos ispravan, program treba da ispita i ispiše da li je uneseni broj manjkav, obilan ili savršen. Nakon toga, program treba da traži unos novog
+broja i da ponavlja postupak sve dok se kao broj ne unese nula. Dijalog između programa i korisnika trebao bi izgledati poput sljedećeg:
+
+    Unesite prirodan broj ili 0 za kraj: 42
+    Broj 42 je obilan!
+    Unesite prirodan broj ili 0 za kraj: 28
+    Broj 28 je savrsen!
+    Unesite prirodan broj ili 0 za kraj: 49
+    Broj 49 je manjkav!
+    Unesite prirodan broj ili 0 za kraj: -1
+    Niste unijeli prirodan broj!
+    Unesite prirodan broj ili 0 za kraj: 2.15
+    Niste unijeli prirodan broj!
+    Unesite prirodan broj ili 0 za kraj: qwerty
+    Niste unijeli prirodan broj!
+    Unesite prirodan broj ili 0 za kraj: 0
+    Dovidjenja!
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a;
+    do{
+        std::cout<<"Unesite prirodan broj ili 0 za kraj: ";
+        std::cin>>a;
+        if(a == 0) {
+            std::cout<<"Dovidjenja!";
+            break;
+        }
+
+        int sum = 0;
+        for(int i = 1; i < a; i++)
+            if(a % i == 0) sum += i;
+        if(sum > a) std::cout<<"Broj "<<a<<" je obilan!\n";
+        else if(sum < a) std::cout<<"Broj "<<a<<" je manjkav!\n";
+             else std::cout<<"Broj "<<a<<" je savrsen!\n";
+    }while(true);
+    return 0;
+}
