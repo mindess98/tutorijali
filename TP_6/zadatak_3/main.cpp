f786b44122fc4b45afacb8a0c2c6e106d8aa9133
mindess98 <mindess98@gmail.com>
2017-08-31 12:18:00 +0200

    tut 6 zad 3 i 4

---
+++ TP_6/zadatak_3/main.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <cmath>
+#include <stdexcept>
+
+using namespace std;
+
+int** KreirajTrougao(int n){
+    if(n < 1) throw domain_error(" ");
+    int** p;
+    try{
+        p = new int*[n];
+        for(int i = 0; i < n; i++){
+            p[i] = new int[1 + 2*i];
+            for(int j = 0; j < 1 + 2*i; j++){
+                p[i][j] = max(i-j+1, j-i+1);
+            }
+        }
+    }
+    catch(bad_alloc& e){
+        throw e;
+    }
+    return p;
+}
+
+int main()
+{
+    int** p;
+
+    cout<<"Koliko zelite redova: ";
+    int n;
+    cin>>n;
+
+    try{
+        p = KreirajTrougao(n);
+    }
+    catch(domain_error& e){
+        cout<<"Izuztak: Broj redova mora biti pozitivan";
+        return 1;
+    }
+    catch(bad_alloc& e){
+        cout<<"Izuzetak: Nedovoljno memorije!";
+        return 1;
+    }
+    catch(...){
+        cout<<"Undefined error";
+        return 1;
+    }
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < 1 + 2*i; j++)
+            cout<<p[i][j]<<" ";
+        cout<<endl;
+    }
+
+    for(int i = 0; i < n; i++)
+        delete[] p[i];
+    delete[] p;
+    return 0;
+}
