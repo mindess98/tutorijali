fc19e38b68dcc3a914edede68b092e92ccf262e6
mindess98 <mindess98@gmail.com>
2017-09-06 17:21:59 +0200

    tut 12 zad 6 1/2

---
+++ TP_12/zadatak_6/main.cpp
@@ -0,0 +1,149 @@
+#include <iostream>
+#include <iomanip>
+#include <cstring>
+#include <stdexcept>
+#include <new>
+
+using namespace std;
+
+template <typename TipEl>
+class Matrica
+{
+    int br_redova, br_kolona;
+    TipEl** elementi;
+    char ime_matrice;
+    static TipEl** AlocirajMemoriju(int br_redova, int br_kolona);
+    static void DealocirajMemoriju(TipEl** elemmenti, int br_redova);
+    void KopirajElemente(TipEl** elementi);
+public:
+    Matrica(int br_redova, int br_kolona, char ime = 0);
+    Matrica(const Matrica& m);
+    Matrica(Matrica&& m);
+    ~Matrica() { DealocirajMemoriju(elementi, br_redova); }
+    Matrica& operator=(const Matrica& m);
+    Matrica& operator=(Matrica&& m);
+    template <typename Tip2>
+    friend Matrica<Tip2> operator+(const Matrica<Tip2>& m1, const Matrica<Tip2>& m2);
+    template <typename Tip2>
+    friend istream& operator>>(istream& is, Matrica<Tip2>& m);
+    template <typename Tip2>
+    friend ostream& operator<<(ostream& os, const Matrica<Tip2>& m);
+};
+
+template <typename TipEl>
+TipEl** Matrica<TipEl>::AlocirajMemoriju(int br_redova, int br_kolona)
+{
+    TipEl** elementi(new TipEl*[br_redova]{});
+    try {
+        for(int i = 0; i < br_redova; i++) elementi[i] = new TipEl[br_kolona];
+    }
+    catch(...) {
+        DealocirajMemoriju(elementi, br_redova);
+        throw;
+    }
+    return elementi;
+}
+template <typename TipEl>
+void Matrica<TipEl>::DealocirajMemoriju(TipEl** elementi, int br_redova)
+{
+    for(int i = 0; i < br_redova; i++) delete[] elementi[i];
+    delete[] elementi;
+}
+template <typename TipEl>
+Matrica<TipEl>::Matrica(int br_redova, int br_kolona, char ime) :
+    br_redova(br_redova), br_kolona(br_kolona), ime_matrice(ime),
+    elementi(AlocirajMemoriju(br_redova, br_kolona)) {}
+template <typename TipEl>
+void Matrica<TipEl>::KopirajElemente(TipEl** elementi)
+{
+    for(int i = 0; i < br_redova; i++)
+        for(int j = 0; j < br_kolona; j++)
+            Matrica::elementi[i][j] = elementi[i][j];
+}
+template <typename TipEl>
+Matrica<TipEl>::Matrica(const Matrica<TipEl>& m) :
+    br_redova(m.br_redova), br_kolona(m.br_kolona), ime_matrice(m.ime_matrice),
+    elementi(AlocirajMemoriju(br_redova, br_kolona))
+{
+    KopirajElemente(m.elementi);
+}
+template <typename TipEl>
+Matrica<TipEl>::Matrica(Matrica<TipEl>&& m) :
+    br_redova(m.br_redova), br_kolona(m.br_kolona), ime_matrice(m.ime_matrice),
+    elementi(AlocirajMemoriju(m.br_redova, m.br_kolona))
+{
+    m.br_redova = 0; m.elementi = nullptr;
+}
+template <typename TipEl>
+Matrica<TipEl>& Matrica<TipEl>::operator=(const Matrica<TipEl>& m)
+{
+    if(br_redova < m.br_redova || br_kolona < m.br_kolona) {
+        TipEl** novi_prostor(AlocirajMemoriju(m.br_redova, m.br_kolona));
+        DealocirajMemoriju(elementi, br_redova);
+        elementi = novi_prostor;
+    }
+    else if(br_redova > m.br_redova)
+        for(int i = m.br_redova; i < br_redova; i++) delete elementi[i];
+    br_redova = m.br_redova; br_kolona = m.br_kolona;
+    ime_matrice = m.ime_matrice;
+    KopirajElemente(m.elementi);
+    return *this;
+}
+template <typename TipEl>
+ostream& operator<<(ostream& os, const Matrica<TipEl>& m)
+{
+    int wi = os.width();
+    for(int i = 0; i < m.br_redova; i++){
+        for(int j = 0; j < m.br_kolona; j++)
+            os<< setw(wi) << m.elementi[i][j];
+        os<<endl;
+    }
+    return os;
+}
+
+template <typename TipEl>
+istream& operator>>(istream& is, Matrica<TipEl>& m)
+{
+    for(int i = 0; i < m.br_redova; i++)
+        for(int j = 0; j < m.br_kolona; j++) {
+            cout<<m.ime_matrice<< "("<<i+1<<","<<j+1<<") = ";
+            is>>m.elementi[i][j];
+        }
+    return is;
+}
+template <typename TipEl>
+Matrica<TipEl>& Matrica<TipEl>::operator=(Matrica<TipEl>&& m)
+{
+    swap(br_redova, m.br_redova); swap(br_kolona, m.br_kolona);
+    swap(ime_matrice, m.ime_matrice); swap(elementi, m.elementi);
+    return *this;
+}
+
+template <typename TipEl>
+Matrica<TipEl> operator+(const Matrica<TipEl>& m1, const Matrica<TipEl>& m2)
+{
+    if(m1.br_redova != m2.br_redova || m1.br_kolona != m2.br_kolona)
+        throw domain_error("Matrice nemaju jednake dimenzije!");
+    Matrica<TipEl> m3(m1.br_redova, m1.br_kolona);
+    for(int i = 0; i < m1.br_redova; i++)
+        for(int j = 0; j < m1.br_kolona; j++)
+            m3.elementi[i][j] = m1.elementi[i][j] + m2.elementi[i][j];
+    return m3;
+}
+
+
+using namespace std;
+
+int main()
+{
+
+    Matrica<double> a(2,2,'A'), b(2,2,'B');
+    cout<<"Unesi matricu A:\n";
+
+    cin>>a;
+    cout<<"Unesi matricu B:\n";
+    cin>>b;
+    cout<<"Zbir ove dvije matrice je:\n";
+    cout<<setw(3)<<a+b;
+    return 0;
+}
